35c69f607dbf23def49bb4f04a7152fae0e6c42c
VinayakGaikwad <vinaayakgaikwad@gmail.com>
2024-11-09 14:04:25 +0530

    final commit

---
+++ next-i18next.config.js
@@ -0,0 +1,8 @@
+// logic for language translation next-i18
+
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["mr", "ru", "ja", "hi", "en", "tl"],
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
 import Image from "next/image";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import Link from "next/link";
+import { LanguageDropDown } from "@/components/LanguageDropDown";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { t } = useTranslation("common");
   return (
     <>
       <div className="bg-gray-50">
@@ -54,7 +49,7 @@ export default function Home() {
               </svg>
             </div>
           </div>
-          <div className="relative pt-6 pb-16 sm:pb-24 ">
+          <div className="relative pt-6 pb-16 sm:pb-24">
             <div className="max-w-7xl mx-auto px-4 sm:px-6">
               <nav className="relative flex items-center justify-between sm:h-10 md:justify-center">
                 <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
@@ -62,9 +57,8 @@ export default function Home() {
                     <Link href="/">
                       <div className="flex items-center">
                         <img
-                          src="readme.svg"
-                          alt=""
                           className="h-8 mt-1 w-auto cursor-pointer"
+                          src="readme.svg"
                         />
                         <p className="text-gray-800 ml-2 -mb-2 text-md">
                           readmeGenerator
@@ -74,33 +68,35 @@ export default function Home() {
                   </div>
                 </div>
                 <div className="relative w-28 items-center flex-1 md:absolute md:inset-y-0 md:right-0 z-10">
-                  Language
+                  <LanguageDropDown />
                 </div>
               </nav>
             </div>
+
             <div className="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
               <div className="text-center">
                 <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                  <span className="block ">Lightning Fast Way to Generate</span>
-                  <span className="block text-green-600">README</span>
+                  <span className="block">{t("title")}</span>
+                  <span className="block text-green-600">{t("readme")}</span>
                 </h1>
                 <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-                  Our editor empowers developers and maintainers to quickly add
-                  and customize all the sections you need for your readme files
+                  {t("description")}
                 </p>
               </div>
-              <div className="flex justify-center mt-6 ">
+              <div className="flex justify-center mt-6">
                 <span className="inline-flex rounded-md shadow">
                   <Link
                     href="/editor"
-                    className="inline-flex items-center px-4 py-2 border border-transparent ext-base font-medium rounded-lg text-green-700 bg-white hover:text-green-600"
+                    className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-lg text-green-600 bg-white hover:text-green-500"
                   >
-                    Get Started
+                    {t("get-started")}
                   </Link>
                 </span>
               </div>
               <div className="text-center">
-                <p className="mt-3 mx-auto text-sm text-gray-900 ">100% Free</p>
+                <p className="mt-3 mx-auto text-sm text-gray-900">
+                  {t("100-free")}
+                </p>
               </div>
             </div>
           </div>
@@ -112,20 +108,20 @@ export default function Home() {
           </div>
           <div className="max-w-7xl mx-auto px-4 sm:px-6">
             <img
+              className="relative rounded-md shadow-lg"
               src="/readmeCreator.png"
-              alt="App Image"
-              className="relative rounded-md shadow-lg "
+              alt="App Screenshot"
             />
           </div>
         </div>
         <div className="bg-gray-800">
           <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
             <h2 className="text-center text-gray-400 text-sm font-semibold tracking-wide">
-              Made With Love &#9825; by{" "}
+              Made with love &#9825; by{" "}
               <a
-                href="https://vinayak-gaikwad-portfolio.vercel.app/"
                 className="hover:text-white"
-                target="_blank"
+                href="https://vinayak-gaikwad-portfolio.vercel.app/"
+                target="__blank"
               >
                 Vinayak Gaikwad
               </a>
@@ -135,4 +131,12 @@ export default function Home() {
       </div>
     </>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
 }
